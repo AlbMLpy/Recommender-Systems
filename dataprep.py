7375c7c445393408b62e0a679abf79a7b78d1316
Evgeny Frolov <evfro@live.ru>
2020-04-25 09:56:56 +0300

    fix test setup: add validation data to training; add experiments with the Yahoo dataset; refactor code slightly

---
+++ dataprep.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def split_holdout(data, userid='userid', feedback=None, sample_max_rated=False, random_state=None):
+    'Randomized sampling of a highly rated item per every user in data'
+    idx_grouper = (
+        data
+        .sample(frac=1, random_state=random_state) # randomly permute data
+        .groupby(userid, as_index=False, sort=False)
+    )
+    if sample_max_rated: # take single item with the highest score
+        idx = idx_grouper[feedback].idxmax()
+    else: # data is already permuted - simply take the 1st element
+        idx = idx_grouper.head(1).index # sample random element
+    
+    observed = data.drop(idx.values)
+    holdout = data.loc[idx.values]
+    return observed, holdout
+
+
+def sample_unseen_items(user_group, item_pool, itemid, n, random_state):
+    'Helper function to run on pandas dataframe grouper'
+    seen_items = user_group[itemid].values
+    candidates = np.setdiff1d(item_pool, seen_items, assume_unique=True)
+    return random_state.choice(candidates, n, replace=False)
+
+
+def sample_unseen_interactions(data, item_pool, userid='userid', itemid='itemid', n_random=999, seed=None):
+    'Randomized sampling of unseen items per every user in data'
+    random_state = np.random if seed is None else np.random.RandomState(seed)
+    return (
+        data
+        .groupby(userid, sort=False)
+        .apply(sample_unseen_items, item_pool, itemid, n_random, random_state)
+    )
